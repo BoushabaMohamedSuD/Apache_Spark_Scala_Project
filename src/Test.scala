e1034048b04ed3ddc7ef097408583e22b9e62d81
mohamedboushaba <med1998yz@gmail.com>
2020-11-29 00:22:13 +0100

    fix hadoop path

--- src/Test.scala
+++ src/Test.scala
@@ -2,10 +2,19 @@
 import org.apache.spark._
 import org.apache.spark.SparkContext
 
+
 object Test {
   def main(args: Array[String]) = {
         println("Hello, world");
-       // var sc=new SparkContext("local[*]","Rating Counter");
+        var sc=new SparkContext("local[*]","RatingsCounter");
+        val lines =sc.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small");
+        println("----------gooooood--------------------");
+        val ratings =lines.map(x=>x.toString().split("\t")(0));
+        val results =ratings.countByValue();
+        val sortedResults=results.toSeq.sortBy(_._1);
+        sortedResults.foreach(println);
+        
+        
     }
   
-}
+} 
