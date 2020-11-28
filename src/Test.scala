d9b5914c19a02012b0a859543443c329ab68f8f0
mohamedboushaba <med1998yz@gmail.com>
2020-11-28 23:12:26 +0100

    add spark

---
+++ src/Test.scala
@@ -0,0 +1,11 @@
+
+import org.apache.spark._
+import org.apache.spark.SparkContext
+
+object Test {
+  def main(args: Array[String]) = {
+        println("Hello, world");
+       // var sc=new SparkContext("local[*]","Rating Counter");
+    }
+  
+}
