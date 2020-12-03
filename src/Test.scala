e3b664e0b510e6da42e8e2d90a6a2bf3798b2f32
mohamedboushaba <med1998yz@gmail.com>
2020-12-03 12:46:34 +0100

    Q1

--- src/Test.scala
+++ src/Test.scala
@@ -7,14 +7,22 @@ object Test {
   def main(args: Array[String]) = {
         println("Hello, world");
         var sc=new SparkContext("local[*]","RatingsCounter");
-        val lines =sc.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small");
+        val lines =sc.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/links.csv");
+        //println(lines);
         println("----------gooooood--------------------");
-        val ratings =lines.map(x=>x.toString().split("\t")(0));
+        /*val ratings =lines.map(x=>x.toString().split(",")(0));
         val results =ratings.countByValue();
         val sortedResults=results.toSeq.sortBy(_._1);
-        sortedResults.foreach(println);
+        sortedResults.foreach(println);*/
         
+        ///// Question 1 ///////
         
+        val links_lines =sc.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/links.csv");
+        val movies_lines =sc.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/movies.csv");
+        val ratings_lines =sc.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/ratings.csv");
+        val tags_lines =sc.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/tags.csv");
+        
+       
     }
   
 } 
