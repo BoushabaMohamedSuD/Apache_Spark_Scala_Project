caae935fbaed18d19453014bcac90882dc411dd8
mohamedboushaba <med1998yz@gmail.com>
2020-12-03 13:16:04 +0100

    Q5

--- src/Test.scala
+++ src/Test.scala
@@ -63,7 +63,26 @@ object Test {
     
     
     
-    ////// Question 6 //////////////////////////////
+    ////// Question 5//////////////////////////////
+    
+    
+    
+    
+    /*
+     * 
+     * Apache Spark ML implements alternating least squares (ALS) for collaborative filtering, a very popular algorithm for making recommendations.
+		 * ALS recommender is a matrix factorization algorithm that uses Alternating Least Squares with Weighted-Lamda-Regularization (ALS-WR).
+		 * It factors the user to item matrix A into the user-to-feature matrix U and the item-to-feature matrix M: It runs the ALS algorithm in a parallel fashion.
+		 * The ALS algorithm should uncover the latent factors that explain the observed user to item ratings and tries ,
+		 * to find optimal factor weights to minimize the least squares between predicted and actual ratings.
+     * 
+     * 
+     * 
+     * 
+     * */
+    
+    
+    //// Question 6 //////////
     
     
     
