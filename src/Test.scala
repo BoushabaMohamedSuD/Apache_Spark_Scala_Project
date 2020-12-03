05ad5dca0bca93bd54a99141f40d8fae7954df8a
mohamedboushaba <med1998yz@gmail.com>
2020-12-03 12:59:45 +0100

    Q2

--- src/Test.scala
+++ src/Test.scala
@@ -1,28 +1,39 @@
 
 import org.apache.spark._
 import org.apache.spark.SparkContext
-
+import org.apache.spark.sql.SparkSession
 
 object Test {
   def main(args: Array[String]) = {
-        println("Hello, world");
-        var sc=new SparkContext("local[*]","RatingsCounter");
-        val lines =sc.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/links.csv");
-        //println(lines);
-        println("----------gooooood--------------------");
-        /*val ratings =lines.map(x=>x.toString().split(",")(0));
+    println("Hello, world");
+    var sc = new SparkContext("local[*]", "RatingsCounter");
+    val lines = sc.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/links.csv");
+    //println(lines);
+    println("----------gooooood--------------------");
+    /*val ratings =lines.map(x=>x.toString().split(",")(0));
         val results =ratings.countByValue();
         val sortedResults=results.toSeq.sortBy(_._1);
         sortedResults.foreach(println);*/
-        
-        ///// Question 1 ///////
-        
-        val links_lines =sc.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/links.csv");
-        val movies_lines =sc.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/movies.csv");
-        val ratings_lines =sc.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/ratings.csv");
-        val tags_lines =sc.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/tags.csv");
-        
-       
-    }
+
+    ///// Question 1 ///////
+
+    val links_lines = sc.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/links.csv");
+    val movies_lines = sc.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/movies.csv");
+    val ratings_lines = sc.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/ratings.csv");
+    val tags_lines = sc.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/tags.csv");
+
+    //// Question 2 ////////////////////
+
+    val ss = SparkSession
+      .builder
+      .appName("retings")
+      .master("local[*]")
+      .getOrCreate();
+
+    val ratings_lines_sql = ss.sparkContext.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/ratings.csv");
+
+    /// Question 3 ///////////////////////////////
   
+  }
+
 } 
