6a0dbc30d9410006929dc1156702782cc2b98b00
mohamedboushaba <med1998yz@gmail.com>
2020-12-03 13:06:46 +0100

    Q3

--- src/Test.scala
+++ src/Test.scala
@@ -33,7 +33,27 @@ object Test {
     val ratings_lines_sql = ss.sparkContext.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/ratings.csv");
 
     /// Question 3 ///////////////////////////////
-  
+
+    /*
+     * Collaborative algorithm uses “User Behaviour” for recommending items.
+     * They exploit behaviour of other users and items in terms of transaction history, ratings, selection and purchase information.
+     * Other users behaviour and preferences over the items are used to recommend items to the new users.
+     *
+     *
+     * The point of content-based is that we have to know the content of both user and item.
+     * Usually we construct user-profile and item-profile using the content of shared attribute space.
+     * For example, for a movie, you represent it with the movie stars in it and the genres (using a binary coding for example).
+     * For user profile, you can do the same thing based on the users likes some movie stars/genres etc.
+     *
+     * */
+     
+    
+    /////// Question 4 ////////////////////////////
+    
+    
+    
+    
+    
   }
 
 } 
