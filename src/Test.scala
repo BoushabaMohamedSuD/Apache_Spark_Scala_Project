0baa443a9362542e8b92b7a69cd7280ba263dcdc
mohamedboushaba <med1998yz@gmail.com>
2020-12-04 17:27:43 +0100

    fix ratings format

--- src/Test.scala
+++ src/Test.scala
@@ -1,7 +1,14 @@
 
 import org.apache.spark._
+
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.mllib.recommendation.Rating
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.recommendation.ALS
+import breeze.macros.expand.args
+import scala.collection.Seq
+import java.lang.Integer
 
 object Test {
   def main(args: Array[String]) = {
@@ -30,8 +37,10 @@ object Test {
       .master("local[*]")
       .getOrCreate();
 
-    val ratings_lines_sql = ss.sparkContext.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/ratings.csv");
+    import ss.implicits._
 
+    val ratings_lines_sql = ss.read.textFile("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/ratings.csv");
+    ratings_lines_sql.show();
     /// Question 3 ///////////////////////////////
 
     /*
@@ -46,48 +55,84 @@ object Test {
      * For user profile, you can do the same thing based on the users likes some movie stars/genres etc.
      *
      * */
-     
-    
+
     /////// Question 4 ////////////////////////////
-    
-    
-    
+
     /*
-     * 
+     *
      * for our case, we have no idea who the user is, all users have been selected randomly,
      * so the best approach system in our case is to use Collaborative Recommendation Filtering,
-     * 
-     * 
-     * 
+     *
+     *
+     *
      * */
-    
-    
-    
+
     ////// Question 5//////////////////////////////
-    
-    
-    
-    
+
     /*
-     * 
+     *
      * Apache Spark ML implements alternating least squares (ALS) for collaborative filtering, a very popular algorithm for making recommendations.
 		 * ALS recommender is a matrix factorization algorithm that uses Alternating Least Squares with Weighted-Lamda-Regularization (ALS-WR).
 		 * It factors the user to item matrix A into the user-to-feature matrix U and the item-to-feature matrix M: It runs the ALS algorithm in a parallel fashion.
 		 * The ALS algorithm should uncover the latent factors that explain the observed user to item ratings and tries ,
 		 * to find optimal factor weights to minimize the least squares between predicted and actual ratings.
-     * 
-     * 
-     * 
-     * 
+     *
+     *
+     *
+     *
      * */
-    
-    
+
     //// Question 6 //////////
+
+    /// first we are going to convert our dataset to dataframe
+
+    // val ratings = ratings_lines_sql.map(func, encoder)
+
+    //val header=ratings_lines_sql.first();
+    //println(header);
+
+    //ratings_lines_sql.show();
+
+    val ratings = ss.read.option("header", "true").csv("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/ratings.csv");
+
+    ratings.show()
+
+    ratings.printSchema();
+    import org.apache.spark.sql.functions._
+    val toInt = udf[Int, String](_.toInt)
+    val toDouble = udf[Double, String](_.toDouble)
+
+    val ratings_Format = ratings.withColumn("userId", toInt(ratings("userId")))
+      .withColumn("movieId", toInt(ratings("movieId")))
+      .withColumn("rating", toDouble(ratings("rating")))
+      .drop("timestamp");
     
-    
-    
-    
-    
+    ratings_Format.printSchema();
+
+    //val ratings =ss.read.json("C:/Users/med19/Desktop/Mohamed/INPT/Spark_Scala/Data/ml-latest-small/ratings.csv").toDF();
+
+    // ratings.show();
+
+    /*val als = new ALS()
+      .setMaxIter(5)
+      .setRegParam(0.01)
+      .setUserCol("userId")
+      .setItemCol("movieId")
+      .setRatingCol("rating")
+
+
+    val model = als.fit(ratings);
+
+
+
+    val userID:Int=args(0).toInt;
+
+    val users=Seq(userID).toDF("userId");
+
+    val ratingsProb=model.recommendForUserSubset(users, 10);
+
+    println(ratingsProb);*/
+
   }
 
 } 
